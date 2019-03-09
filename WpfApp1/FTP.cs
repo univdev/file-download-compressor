814f0d5901a1030e20a5e49c8bac71d63710d500
박찬영 <rhdwkrxla68@gmail.com>
2019-03-09 10:33:36 +0900

    디렉토리 다운로드

---
+++ WpfApp1/FTP.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    class FTP
+    {
+        string host = null;
+        string location = null;
+        string username = null;
+        string password = null;
+
+        public FTP (string host, string location, string username, string password)
+        {
+            this.host = host;
+            this.location = location;
+            this.username = username;
+            this.password = password;
+        }
+        public void DirectoryDownload (string root, string DirectoryName, string destination)
+        {
+            // Get the object used to communicate with the server.
+            Uri uri = new Uri(host + "/" + root + "/" + DirectoryName);
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(uri);
+            request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+
+            // This example assumes the FTP site uses anonymous logon.
+            request.Credentials = new NetworkCredential(username, password);
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            string resource = null;
+            while ((resource = reader.ReadLine()) != null)
+            {
+                string directory = root + "/" + DirectoryName;
+                /// 파일질라 테스트/20190309
+                string fileName = Path.GetFileName(resource).Substring(3);
+                string downloadDirectory = destination + "/" + DirectoryName;
+                if (!Directory.Exists(downloadDirectory)) Directory.CreateDirectory(downloadDirectory);
+                if (fileName.IndexOf('.') < 0)
+                    this.DirectoryDownload(directory, fileName, downloadDirectory);
+                else
+                    this.FileDownload(directory, fileName, downloadDirectory);
+            }
+
+            reader.Close();
+            response.Close();
+        }
+        public void FileDownload (string directory, string filename, string downloadDirectory)
+        {
+            try
+            {
+                Uri uri = new Uri(this.host + "/" + directory + "/" + filename);
+
+                using (WebClient request = new WebClient())
+                {
+                    request.Credentials = new NetworkCredential(this.username, this.password);
+                    request.DownloadFile(uri, downloadDirectory + "/" + filename);
+                }
+            } catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+        public void Compress(string path, string directoryName)
+        {
+            // Get the object used to communicate with the server.
+            string folderPath = path + "/" + directoryName;
+            string zipPath = path + "/" + directoryName + ".zip";
+            ZipFile.CreateFromDirectory(folderPath, zipPath);
+        }
+        public void DeleteDirectory(string path)
+        {
+            string[] files = Directory.GetFiles(path);
+            string[] dirs = Directory.GetDirectories(path);
+
+            foreach (string file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+            foreach (string dir in dirs)
+            {
+                DeleteDirectory(dir);
+            }
+
+            Directory.Delete(path, false);
+        }
+        public void exec(string path, string date, string destination)
+        {
+            this.DirectoryDownload(path, date, destination);
+            this.Compress(destination, date);
+            this.DeleteDirectory(destination + "/" + date);
+            MessageBox.Show("파일 다운로드가 완료되었습니다.");
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DownloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameInput.Text;
+            string password = PasswordInput.Password;
+            string host = "ftp://" + HostInput.Text;
+            string location = HostInput.Text;
+            string date = DateInput.Text.Replace("-", "");
+            string download = DownloadInput.Text;
+
+            FTP ftp = new FTP(host, location, username, password);
+            ftp.exec("/", date, download);
+        }
+    }
+}
